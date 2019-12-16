63b97d690e4150db6420c68bb7668098a359f67f
Phong Yang <phongyang88@hotmail.com>
2019-12-16 16:38:12 -0600

    finished mvp. completed stretch goal refactor movieCard

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import {Route} from "react-router-dom";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 import SavedList from './Movies/SavedList';
 
@@ -12,8 +15,14 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/">
+        <MovieList />
+      </Route>
+      <Route path="/movies/:id">
+        <Movie />
+      </Route>
     </div>
+    
   );
 };
 
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {useParams} from "react-router-dom";
+import MovieCard from "./MovieCard";
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
+  const {id} = useParams();
+  console.log(id);
  
   useEffect(() => {
-    const id = 1;
+    // const id = 1;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -29,26 +33,10 @@ const Movie = (props) => {
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
-
-  const { title, director, metascore, stars } = movie;
+  
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      {MovieCard(movie)};
       <div className="save-button">Save</div>
     </div>
   );
